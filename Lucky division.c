9b55e4567153889f7e3e744028c32581a2d1c63e
Siddharth Gupta <72133884+Siddharthh06@users.noreply.github.com>
2021-08-17 01:43:16 +0530

    Add files via upload

---
+++ Lucky division.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char num[1000];
+    scanf("%s", num);
+    int len;
+    len = strlen(num);
+    int numint =  atoi(num);
+    
+    for (int i = 0; i<len; i++)
+    {
+        if (num[i] == '4' && num[i+1] == '7')
+        {
+            printf("YES");
+            exit(0);
+        }
+        else
+        {
+            if(numint%47 == 0)
+            {
+                printf("YES");
+                exit(0);
+            }
+            else if(numint%7 == 0)
+            {
+                printf("YES");
+                exit(0);
+            }
+            else if (numint%4 == 0)
+            {
+                printf("YES");
+                exit(0);
+            }
+            else
+            {
+                printf("NO");
+                break;
+            }
+        }
+    }
+    
+    
+    return 0;
+}
+
+
